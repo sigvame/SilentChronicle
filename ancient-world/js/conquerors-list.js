87df1f207294951a466975880122c792072f919e
sigvame <157107580+sigvame@users.noreply.github.com>
2026-07-30 12:50:56 +0200

    Add files via upload

---
+++ ancient-world/js/conquerors-list.js
@@ -0,0 +1,11 @@
+const conquerorsPlaces = [
+    {
+        title: "Гай Юлий Цезарь (Gaius Julius Caesar)",
+        categories: ["rome", "conquerors"],
+        tag: "Рим • Полководец",
+        country: "🏛️ Римская республика / Галлия",
+        desc: "Выдающийся полководец и политик, покоривший Галлию и изменивший ход римской истории.",
+        image: "images/julius-caesar/caesar-portrait-main.jpg",
+        link: "pages/conquerors/julius-caesar.html"
+    }
+];
