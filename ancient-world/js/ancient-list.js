fc81f82852e6d4b70cdb9c85a35d4e960376b77a
sigvame <157107580+sigvame@users.noreply.github.com>
2026-08-06 16:12:34 +0200

    Update ancient-list.js

--- ancient-world/js/ancient-list.js
+++ ancient-world/js/ancient-list.js
@@ -1,47 +1,47 @@
 const ancientPlaces = [
     {
-        title: "Римская империя (Senatus Populusque Romanus)",
-        categories: ["europe", "empires", "antic"],
-        tag: "Европа • Античность • Империя",
-        country: "🌍 Территории более 40 стран",
-        desc: "Величайшая империя античности: Европа, Азия и Африка под властью Рима.",
-        image: "images/roman-empire/roman-empire-main.jpg",
-        link: "pages/empires/roman-empire.html"
-      },
-      {
-        title: "Британская империя",
-        categories: ["europe", "empires"],
-        tag: "Европа • Империя",
-        country: "📍 Великобритания",
-        desc: "Самое крупное государство в истории человечества, охватывавшее четверть всей суши. История колониальной сверхдержавы, над которой никогда не заходило солнце.",
-        image: "images/british-empire/british-empire-lead.jpg",
-        link: "pages/empires/british-empire.html"
-      },
-      {
-        title: "Троя (Илион)",
-        categories: ["wonders-old"], 
-        tag: "Древние чудеса • Бронзовый век",
-        country: "📍 Турция",
-        desc: "Легендарный город Гомера, считавшийся вымыслом, пока не был раскопан Генрихом Шлиманом. Место эпической Троянской войны, где мифы встретились с реальностью.",
-        image: "images/troy/troy-lead.jpg",
-        link: "pages/wonders_ancient/troy.html"
-      },
-      {
-        title: "Урук (Город Гильгамеша)",
-        categories: ["wonders-old", "mesopotamia"], 
-        tag: "Древние чудеса • Месопотамия",
-        country: "📍 Современный Ирак",
-        desc: "Первый город и мегаполис в истории человечества. Родина клинописи, колеса и легендарного царя-героя Гильгамеша.",
-        image: "images/uruk/uruk-lead.jpg",
-        link: "pages/wonders_ancient/uruk.html"
-      },
-      {
-        title: "Вавилон (Babylon)",
-        categories: ["wonders-old", "mesopotamia"],
-        tag: "Древние чудеса • Месопотамия",
-        country: "📍 Современный Ирак",
-        desc: "Легендарная столица Ближнего Востока на берегах Евфрата. Город лазурных ворот Иштар, первого в истории свода законов и знаменитых садов Семирамиды.",
-        image: "images/babylon/babylon-lead.jpg",
-        link: "pages/wonders_ancient/babylon.html"
-      }
+      title: "Римская империя (Senatus Populusque Romanus)",
+      categories: ["europe", "empires", "antic"],
+      tag: "Европа • Античность • Империя",
+      country: "🌍 Территории более 40 стран",
+      desc: "Величайшая империя античности: Европа, Азия и Африка под властью Рима.",
+      image: "images/roman-empire/roman-empire-main.jpg",
+      link: "pages/empires/roman-empire.html"
+    },
+    {
+      title: "Британская империя",
+      categories: ["europe", "empires"],
+      tag: "Европа • Империя",
+      country: "📍 Великобритания",
+      desc: "Самое крупное государство в истории человечества, охватывавшее четверть всей суши. История колониальной сверхдержавы, над которой никогда не заходило солнце.",
+      image: "images/british-empire/british-empire-lead.jpg",
+      link: "pages/empires/british-empire.html"
+    },
+    {
+      title: "Троя (Илион)",
+      categories: ["wonders-old"], 
+      tag: "Древние чудеса • Бронзовый век",
+      country: "📍 Турция",
+      desc: "Легендарный город Гомера, считавшийся вымыслом, пока не был раскопан Генрихом Шлиманом. Место эпической Троянской войны, где мифы встретились с реальностью.",
+      image: "images/troy/troy-lead.jpg",
+      link: "pages/wonders_ancient/troy.html"
+    },
+    {
+      title: "Урук (Город Гильгамеша)",
+      categories: ["wonders-old", "mesopotamia"], 
+      tag: "Древние чудеса • Месопотамия",
+      country: "📍 Современный Ирак",
+      desc: "Первый город и мегаполис в истории человечества. Родина клинописи, колеса и легендарного царя-героя Гильгамеша.",
+      image: "images/uruk/uruk-lead.jpg",
+      link: "pages/wonders_ancient/uruk.html"
+    },
+    {
+      title: "Вавилон (Babylon)",
+      categories: ["wonders-old", "mesopotamia"],
+      tag: "Древние чудеса • Месопотамия",
+      country: "📍 Современный Ирак",
+      desc: "Легендарная столица Ближнего Востока на берегах Евфрата. Город лазурных ворот Иштар, первого в истории свода законов и знаменитых садов Семирамиды.",
+      image: "images/babylon/babylon-lead.jpg",
+      link: "pages/wonders_ancient/babylon.html"
+    }     
 ];
