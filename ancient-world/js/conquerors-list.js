612fc310ba99cb1a266e737195ab762e23ed7ccf
sigvame <157107580+sigvame@users.noreply.github.com>
2026-08-10 14:29:59 +0200

    Update conquerors-list.js

--- ancient-world/js/conquerors-list.js
+++ ancient-world/js/conquerors-list.js
@@ -7,5 +7,14 @@ const conquerorsPlaces = [
         desc: "Выдающийся полководец и политик, покоривший Галлию и изменивший ход римской истории.",
         image: "images/julius-caesar/caesar-portrait-main.jpg",
         link: "pages/conquerors/julius-caesar.html"
-    }
+    },
+    {
+        title: "Октавиан Август (Gaius Julius Caesar Augustus)",
+        categories: ["rome", "conquerors"],
+        tag: "Рим • Император",
+        country: "🏛️ Римская империя",
+        desc: "Первый римский император, основатель принципата и архитектор эпохи «Pax Romana», положивший конец гражданским войнам.",
+        image: "images/octavian-augustus/augustus-portrait.jpg",
+        link: "pages/conquerors/octavian-augustus.html"
+      }
 ];
