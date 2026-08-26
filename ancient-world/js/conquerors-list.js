33a7fb47b6a79920badda33b641aac316ce1b1d4
sigvame <157107580+sigvame@users.noreply.github.com>
2026-08-26 16:01:33 +0200

    Update conquerors-list.js

--- ancient-world/js/conquerors-list.js
+++ ancient-world/js/conquerors-list.js
@@ -4,6 +4,7 @@ const conquerorsPlaces = [
         categories: ["rome", "conquerors"],
         tag: "Рим • Полководец",
         country: "🏛️ Римская республика / Галлия",
+        date: "2026-07-30",
         desc: "Выдающийся полководец и политик, покоривший Галлию и изменивший ход римской истории.",
         image: "images/julius-caesar/caesar-portrait-main.jpg",
         link: "pages/conquerors/julius-caesar.html"
@@ -13,6 +14,7 @@ const conquerorsPlaces = [
         categories: ["rome", "conquerors"],
         tag: "Рим • Император",
         country: "🏛️ Римская империя",
+        date: "2026-08-10",
         desc: "Первый римский император, основатель принципата и архитектор эпохи «Pax Romana», положивший конец гражданским войнам.",
         image: "images/octavian-augustus/augustus-portrait.jpg",
         link: "pages/conquerors/octavian-augustus.html"
