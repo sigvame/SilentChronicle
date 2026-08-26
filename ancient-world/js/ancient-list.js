561cb296d30a1388669bcac122ba09e636bf467b
sigvame <157107580+sigvame@users.noreply.github.com>
2026-08-26 15:57:19 +0200

    Update ancient-list.js

--- ancient-world/js/ancient-list.js
+++ ancient-world/js/ancient-list.js
@@ -4,6 +4,7 @@ const ancientPlaces = [
       categories: ["europe", "empires", "antic"],
       tag: "Европа • Античность • Империя",
       country: "🌍 Территории более 40 стран",
+      date: "2026-07-29",
       desc: "Величайшая империя античности: Европа, Азия и Африка под властью Рима.",
       image: "images/roman-empire/roman-empire-main.jpg",
       link: "pages/empires/roman-empire.html"
@@ -13,6 +14,7 @@ const ancientPlaces = [
       categories: ["europe", "empires"],
       tag: "Европа • Империя",
       country: "📍 Великобритания",
+      date: "2026-08-02",
       desc: "Самое крупное государство в истории человечества, охватывавшее четверть всей суши. История колониальной сверхдержавы, над которой никогда не заходило солнце.",
       image: "images/british-empire/british-empire-lead.jpg",
       link: "pages/empires/british-empire.html"
@@ -22,6 +24,7 @@ const ancientPlaces = [
       categories: ["wonders-old"], 
       tag: "Древние чудеса • Бронзовый век",
       country: "📍 Турция",
+      date: "2026-08-04",
       desc: "Легендарный город Гомера, считавшийся вымыслом, пока не был раскопан Генрихом Шлиманом. Место эпической Троянской войны, где мифы встретились с реальностью.",
       image: "images/troy/troy-lead.jpg",
       link: "pages/wonders_ancient/troy.html"
@@ -31,6 +34,7 @@ const ancientPlaces = [
       categories: ["wonders-old", "mesopotamia"], 
       tag: "Древние чудеса • Месопотамия",
       country: "📍 Современный Ирак",
+      date: "2026-08-05",
       desc: "Первый город и мегаполис в истории человечества. Родина клинописи, колеса и легендарного царя-героя Гильгамеша.",
       image: "images/uruk/uruk-lead.jpg",
       link: "pages/wonders_ancient/uruk.html"
@@ -40,6 +44,7 @@ const ancientPlaces = [
       categories: ["wonders-old", "mesopotamia"],
       tag: "Древние чудеса • Месопотамия",
       country: "📍 Современный Ирак",
+      date: "2026-08-06",
       desc: "Легендарная столица Ближнего Востока на берегах Евфрата. Город лазурных ворот Иштар, первого в истории свода законов и знаменитых садов Семирамиды.",
       image: "images/babylon/babylon-lead.jpg",
       link: "pages/wonders_ancient/babylon.html"
@@ -49,6 +54,7 @@ const ancientPlaces = [
       categories: ["wonders-old"],
       tag: "Древние чудеса • Ближний Восток",
       country: "📍 Палестина",
+      date: "2026-08-06",
       desc: "Древнейшие укрепленное поселение в истории, возникшие около 9 000 лет назад. Родина первого каменного небоскреба и легендарных Иерихонских стен.",
       image: "images/jericho/jericho-lead.jpg",
       link: "pages/wonders_ancient/jericho.html"
@@ -58,6 +64,7 @@ const ancientPlaces = [
       categories: ["empires"],
       tag: "Древний мир • Империя",
       country: "🏛️ Македония / Греция",
+      date: "2026-08-12",
       desc: "Сверхдержава древности, созданная Александром Великим. Государство, объединившее Запад и Восток и положившее начало эпохе эллинизма.",
       image: "images/macedon/macedon-main.jpg",
       link: "pages/empires/macedonian-empire.html"
